5fe542e221f28ea603e9e16683a907e4ad47bd91
Guilherme <guilhermet213@gmail.com>
2022-05-14 22:44:45 -0300

    add profile component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="wrapper">
+      <h1>Guilherme Tavares</h1>
+      <h2>Desenvolvedor Front-end</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, ipsa
+        aut! Quam nesciunt eveniet facere tenetur itaque praesentium omnis.
+      </p>
+    </div>
+  );
+};
+
+export default Profile;
