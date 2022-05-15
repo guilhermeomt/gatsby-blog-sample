21f0a7d700b344b29fd8b4b0bc229591d372f452
Guilherme <guilhermet213@gmail.com>
2022-05-15 12:28:49 -0300

    add styles to blog post template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,17 +1,27 @@
 import React from "react";
 import { graphql } from "gatsby";
+import Layout from "../components/Layout";
+import Seo from "../components/seo";
+import * as S from "../components/Post/styles";
 
 const BlogPost = ({ data }) => {
   const { title, date, description } = data.markdownRemark.frontmatter;
-  const html = data.markdownRemark.html;
+  const { html, timeToRead } = data.markdownRemark;
 
   return (
-    <>
-      <h1>{title}</h1>
-      <p>{date}</p>
-      <p>{description}</p>
-      <div dangerouslySetInnerHTML={{ __html: html }} />
-    </>
+    <Layout>
+      <Seo title={title} description={description} />
+      <S.PostHeader>
+        <S.PostDate>
+          {date} • {timeToRead} min de leitura
+        </S.PostDate>
+        <S.PostTitle>{title}</S.PostTitle>
+        <S.PostDescription>{description}</S.PostDescription>
+      </S.PostHeader>
+      <S.MainContent>
+        <div dangerouslySetInnerHTML={{ __html: html }}></div>
+      </S.MainContent>
+    </Layout>
   );
 };
 
@@ -20,10 +30,11 @@ export const query = graphql`
     markdownRemark(fields: { slug: { eq: $slug } }) {
       frontmatter {
         title
-        date
+        date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
         description
       }
       html
+      timeToRead
     }
   }
 `;
